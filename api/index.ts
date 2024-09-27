93338d7095237e806be86dd28238d57bb4e9b537
Sover <thesoverman@gmail.com>
2024-09-27 16:46:18 +0300

    deploy atempt 5

--- api/index.ts
+++ api/index.ts
@@ -1,29 +1,29 @@
-import express, { Request, Response } from 'express';
+import 'dotenv/config';
+import express, { Express } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
-import dotenv from "dotenv"
+import router from '../src/router/index';
+import errorMiddleware from '../src/middlewares/error-middleware';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-dotenv.config();
+const PORT: number = Number(process.env.PORT) || 5000;
+const app: Express = express();
+
 app.use(express.json());
-
-const mongoURL = process.env.DB_URL;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
-});
-
+app.use(cookieParser());
+app.use(cors());
+app.use('/authapi', router);
+app.use('/marketapi', router)
+app.use(errorMiddleware);
 
 const start = async () => {
-  try {
-    if (!mongoURL) {
-      throw new Error('DB_URL is not defined in the environment variables');
+    try {
+        await mongoose.connect(process.env.DB_URL as string);
+        app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
+    } catch (e) {
+        console.error(e);
     }
-    await mongoose.connect(mongoURL)
-    console.log("connected to db")
-  } catch (error) {
-    console.log(error)
-  }
-}
-start()
+};
+
+start();
 module.exports = app;
