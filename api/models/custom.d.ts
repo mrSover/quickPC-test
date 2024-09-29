11edf7bb20e35fea1659501c66fbcb5d12bcd830
Sover <145704673+mrSover@users.noreply.github.com>
2024-09-29 18:23:24 +0300

    Delete api/models directory

--- api/models/custom.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Express {
-    export interface Request {
-        file?: Express.Multer.File;
-        files?: { [fieldname: string]: Express.Multer.File[] } | Express.Multer.File[]; // Врахування обох варіантів
-    }
-}
