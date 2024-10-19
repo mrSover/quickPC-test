6faa05e6cedad535ff7fa00c0ed7a32c5a82b467
Sover <145704673+mrSover@users.noreply.github.com>
2024-10-19 16:20:16 +0300

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -1,50 +1,29 @@
-// import 'dotenv/config';
-// import express, { Express } from 'express';
-// import cors from 'cors';
-// import cookieParser from 'cookie-parser';
-// import mongoose from 'mongoose';
-// import router from '../api/router/index';
-// import errorMiddleware from '../api/middlewares/error-middleware';
-
-// const PORT: number = Number(process.env.PORT) || 5000;
-// const app: Express = express();
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(cors());
-// app.use('/api', router);
-// app.use(errorMiddleware);
-
-// const start = async () => {
-//     try {
-//         await mongoose.connect(process.env.DB_URL as string);
-//         app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
-//     } catch (e) {
-//         console.error(e);
-//     }
-// };
-
-// start();
-
-
-import express, { Express, Request, Response } from 'express';
+import 'dotenv/config';
+import express, { Express } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import router from '../api/router/index';
+import errorMiddleware from '../api/middlewares/error-middleware';
 
 const PORT: number = Number(process.env.PORT) || 5000;
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', router);
+app.use(errorMiddleware);
 
-app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL as string);
+        app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
+    } catch (e) {
+        console.error(e);
+    }
+};
 
-// const start = async () => {
-//   try {
-//   } catch (e) {
-//     console.error(e);
-//   }
-// };
+start();
 
-//  start();
 module.exports = app;
