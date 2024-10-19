220559ce3935d4b0d7a30d2b02e0970c49218fc8
Sover <145704673+mrSover@users.noreply.github.com>
2024-10-19 17:40:41 +0300

    Update product-catalog-dto.ts

--- backend/api/dtos/product-catalog-dto.ts
+++ backend/api/dtos/product-catalog-dto.ts
@@ -1,8 +1,8 @@
 import { ObjectId } from 'mongodb';
 
 export default class ProductCatalog {
-    _id: ObjectId
-    category: string
+    _id: ObjectId;
+    category: string;
     name: string;
     type: string;
     price: number;
@@ -10,15 +10,16 @@ export default class ProductCatalog {
     is_hot: boolean;
     img: string;
 
+    [key: string]: any;
 
     constructor(model: ProductCatalog) {
-        this._id = model._id
-        this.category = model.category
-        this.name = model.name
-        this.type = model.type
-        this.price = model.price
-        this.is_on_sale = model.is_on_sale
-        this.is_hot = model.is_hot
-        this.img = model.img
+        this._id = model._id;
+        this.category = model.category;
+        this.name = model.name;
+        this.type = model.type;
+        this.price = model.price;
+        this.is_on_sale = model.is_on_sale;
+        this.is_hot = model.is_hot;
+        this.img = model.img;
     }
-} 
+}
